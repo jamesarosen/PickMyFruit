f8c2fd03f12ead17f6a225cee7a54d70662f92c6
James A Rosen <pickmyfruit@jamesarosen.com>
2024-12-25 14:58:54 -0800

    chore(CI) publish DNS records after deploy
    - add `dns` package with a Terraform configuration for the DNS records
    - add `terraform format` and `terraform validate` to the `lint` workflow
    - move Fly.io GitHub action into a new `deploy` workflow
    - add `terraform apply` to the `deploy` workflow

---
+++ packages/dns/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    dnsimple = {
+      source  = "dnsimple/dnsimple"
+      version = "1.8.0"
+    }
+  }
+}
+
+variable "dnsimple_account" {
+  type      = number
+  ephemeral = true
+  nullable  = false
+}
+
+variable "dnsimple_token" {
+  type      = string
+  ephemeral = true
+  nullable  = false
+}
+
+variable "dnsimple_zone" {
+  type     = string
+  nullable = false
+}
+
+provider "dnsimple" {
+  token   = var.dnsimple_token
+  account = var.dnsimple_account
+}
+
+resource "dnsimple_zone_record" "www_cname" {
+  zone_name = var.dnsimple_zone
+  name      = "www"
+  type      = "CNAME"
+  value     = "pickmyfruit.fly.dev"
+}
+
+resource "dnsimple_zone_record" "apex_redirect" {
+  zone_name = var.dnsimple_zone
+  name      = ""
+  type      = "URL"
+  value     = "https://www.${var.dnsimple_zone}"
+}
